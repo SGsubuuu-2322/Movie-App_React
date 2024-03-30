ca7ad3024584fa4857045eec23e0f3e228473983
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-30 23:46:30 +0530

    Installed some of the major packages that are gonna needed in further development...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // import React from 'react'
 
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
