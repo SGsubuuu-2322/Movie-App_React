d9d6b0e779c59c2d0a307f1282fd8f528dab33db
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-01 00:04:49 +0530

    Added search bar and box functionality through React js...

--- src/Components/Templates/TopNav.jsx
+++ src/Components/Templates/TopNav.jsx
@@ -1,50 +1,32 @@
 // import React from 'react'
 
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const TopNav = () => {
+  const [query, setQuery] = useState("");
   return (
-    <div className="relative w-full h-[10vh] flex justify-center items-center">
+    <div className="relative w-full h-[10vh] flex justify-start items-center ml-[15%]">
       <i className="text-3xl text-zinc-400 ri-search-line"></i>
       <input
+        onChange={(e) => setQuery(e.target.value)}
         className="w-[50%] mx-10 p-2 outline-none text-xl border-none bg-transparent text-zinc-200"
         type="text"
+        value={query}
         placeholder="Search anything..."
       />
-      <i className="text-3xl text-zinc-400 ri-close-fill"></i>
-      <div className="bg-zinc-200 w-[50%] h-[50vh] absolute top-[90%] overflow-auto">
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
+      {query && (
+        <i
+          onClick={() => setQuery("")}
+          className="text-3xl text-zinc-400 ri-close-fill"
+        ></i>
+      )}
+
+      <div className="bg-zinc-200 w-[50%] max-h-[50vh] absolute top-[90%] overflow-auto">
+        {/* <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
           <img src="" alt="" />
           <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
-        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
-          <img src="" alt="" />
-          <span>Hello Everyone....</span>
-        </Link>
+        </Link> */}
       </div>
     </div>
   );
