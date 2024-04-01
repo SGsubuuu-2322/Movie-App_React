96a4b5d50660729dccf88aca25a32ad2afc151ef
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-02 00:39:08 +0530

    Debugged some stylings...

--- src/Components/Templates/TopNav.jsx
+++ src/Components/Templates/TopNav.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const TopNav = () => {
   const [query, setQuery] = useState("");
   return (
-    <div className="relative w-full h-[10vh] flex justify-start items-center ml-[15%]">
+    <div className="relative w-full h-[10vh] flex justify-start items-center">
       <i className="text-3xl text-zinc-400 ri-search-line"></i>
       <input
         onChange={(e) => setQuery(e.target.value)}
@@ -23,10 +23,10 @@ const TopNav = () => {
       )}
 
       <div className="bg-zinc-200 w-[50%] max-h-[50vh] absolute top-[90%] overflow-auto">
-        {/* <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
+        <Link className="hover:text-black hover:bg-zinc-300 duration-300 w-[100%] p-10 flex justify-start items-center border-b-2 border-zinc-100 text-zinc-600 font-semibold">
           <img src="" alt="" />
           <span>Hello Everyone....</span>
-        </Link> */}
+        </Link>
       </div>
     </div>
   );
