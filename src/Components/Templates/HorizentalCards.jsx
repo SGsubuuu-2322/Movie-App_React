b65226ed20f7e8b91d428af7d16a4c4a91ff9549
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-06 23:46:10 +0530

    modified some of the horizental cards stylings...

--- src/Components/Templates/HorizentalCards.jsx
+++ src/Components/Templates/HorizentalCards.jsx
@@ -3,14 +3,17 @@
 const HorizentalCards = ({ data }) => {
   //   console.log(data);
   return (
-    <div className="w-full h-[40vh] p-5">
+    <div className="w-full  p-5">
       <div className="mb-5">
         <h1 className="text-3xl text-zinc-400 font-bold">Trending</h1>
       </div>
 
-      <div className="w-[100%] h-[40vh] flex overflow-y-hidden">
+      <div className="w-[100%] flex overflow-y-hidden">
         {data.map((d, i) => (
-          <div key={i} className="min-w-[15%] mr-5 bg-zinc-900">
+          <div
+            key={i}
+            className="min-w-[15%] mr-5 mb-2 bg-zinc-900 pb-2 overflow-hidden"
+          >
             <img
               className="w-full h-[55%] object-cover"
               src={`https://image.tmdb.org/t/p/original/${
@@ -18,14 +21,17 @@ const HorizentalCards = ({ data }) => {
               }`}
               alt=""
             />
-            <h1 className="text-xl font-black text-white">
-              {" "}
-              {d.name || d.original_name || d.title || d.original_title}
-            </h1>
-            <p className="text-white mt-3 mb-3">
-              {d.overview.slice(0, 50)}...
-              <span className="text-blue-600">more</span>
-            </p>
+
+            <div className="text-white p-1 h-[30%]">
+              <h1 className=" mt-3 text-xl font-semibold ">
+                {" "}
+                {d.name || d.original_name || d.title || d.original_title}
+              </h1>
+              <p>
+                {d.overview.slice(0, 50)}...
+                <span className="text-zinc-500">more</span>
+              </p>
+            </div>
           </div>
         ))}
       </div>
