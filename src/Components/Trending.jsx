f8175445340bdd0c55ff442f314988bf33936703
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-10 15:57:20 +0530

    Created the navbar for our trending page...

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -1,10 +1,24 @@
 // import React from 'react'
+import { useNavigate } from "react-router-dom";
+import TopNav from "./Templates/TopNav";
+import Dropdown from "./Templates/Dropdown";
 
 const Trending = () => {
+  const Navigate = useNavigate();
   return (
-    <div className="h-screen w-screen">
-      <div className="h-[10vh] w-full bg-red-200 flex items-center">
-        <h1>Trending</h1>
+    <div className="p-[3%] h-screen w-screen">
+      <div className="w-full flex items-center">
+        <h1 className="w-[20%] text-2xl font-semibold text-zinc-400">
+          <i
+            onClick={() => Navigate(-1)}
+            className="hover:text-[#6556cd] ri-arrow-left-line"
+          ></i>
+          Trending
+        </h1>
+        <TopNav />
+        <Dropdown title="category" options={["movie", "tv", "all"]} />
+        <div className="w-[2%]"></div>
+        <Dropdown title="duration" options={["week", "day"]} />
       </div>
     </div>
   );
