e53cb330db72f98fbda6fea6f7326645a633c495
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-10 17:08:22 +0530

    Added some api call codes for trending component...

--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -2,12 +2,34 @@
 import { useNavigate } from "react-router-dom";
 import TopNav from "./Templates/TopNav";
 import Dropdown from "./Templates/Dropdown";
+import { useEffect, useState } from "react";
+import axios from "../Utils/Axios";
 
 const Trending = () => {
   const Navigate = useNavigate();
+  const [category, setCategory] = useState("all");
+  const [duration, setDuration] = useState("day");
+  const [trending, setTrending] = useState(null);
+
+  const getTrending = async () => {
+    try {
+      const { data } = await axios.get(`/trending/${category}/${duration}`);
+      setTrending(data.results);
+      // console.log(randomWallpaper);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
+
+  // console.log(trending);
+
+  useEffect(() => {
+    getTrending();
+  }, [category, duration]);
+
   return (
-    <div className="p-[3%] h-screen w-screen">
-      <div className="w-full flex items-center">
+    <div className="px-[3%] h-screen w-screen">
+      <div className="w-full flex items-center justify-between">
         <h1 className="w-[20%] text-2xl font-semibold text-zinc-400">
           <i
             onClick={() => Navigate(-1)}
@@ -15,10 +37,12 @@ const Trending = () => {
           ></i>
           Trending
         </h1>
-        <TopNav />
-        <Dropdown title="category" options={["movie", "tv", "all"]} />
-        <div className="w-[2%]"></div>
-        <Dropdown title="duration" options={["week", "day"]} />
+        <div className="flex items-center w-[80%]">
+          <TopNav />
+          <Dropdown title="category" options={["movie", "tv", "all"]} />
+          <div className="w-[2%]"></div>
+          <Dropdown title="duration" options={["week", "day"]} />
+        </div>
       </div>
     </div>
   );
