ee8ba1076c2073a0ee14f39bbdd69d75e0dded1e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-11 16:10:57 +0530

    Created the ratings on the movies and tv-shows and debugged some cards code...

--- src/Components/Templates/Cards.jsx
+++ src/Components/Templates/Cards.jsx
@@ -3,10 +3,11 @@
 import { Link } from "react-router-dom";
 
 const Cards = ({ data, title }) => {
+  console.log(data);
   return (
     <div className="flex flex-wrap w-full px-[5%] pt-[1%] bg-[#1f1e24]">
       {data.map((d, i) => (
-        <Link className="w-[25vh] mr-[5%] mb-[5%]" key={i}>
+        <Link className="relative w-[25vh] mr-[5%] mb-[5%]" key={i}>
           <img
             className="shadow-[8px_17px_38px_2px_rgba(0,0,0,0.6)] h-[40vh] object-cover"
             src={`https://image.tmdb.org/t/p/original/${
@@ -17,6 +18,12 @@ const Cards = ({ data, title }) => {
           <h1 className="text-2xl text-zinc-300 font-semibold mt-3">
             {d.name || d.original_name || d.title || d.original_title}
           </h1>
+
+          {d.vote_average && (
+            <div className="absolute bottom-[35%] right-[-12%] text-xl text-white font-semibold h-[6vh] w-[6vh] flex justify-center items-center bg-yellow-400 rounded-full">
+              {(d.vote_average * 10).toFixed()} <sup>%</sup>
+            </div>
+          )}
         </Link>
       ))}
     </div>
