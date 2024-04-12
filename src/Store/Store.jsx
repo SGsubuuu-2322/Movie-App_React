a2bace3404d898ca9699e7e02806f8807a662c5c
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-12 10:46:48 +0530

    Added all the reducers to our store...

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import MovieReducer from "./reducers/MovieSlice";
+import TvReducer from "./reducers/TvSlice";
+import PersonReducer from "./reducers/PersonSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    movie: MovieReducer,
+    tv: TvReducer,
+    person: PersonReducer,
+  },
 });
