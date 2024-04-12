fd2d69a8427dda63999234323939aed2116415a9
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-12 10:22:14 +0530

    Configured the store in our app for using redux...

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { store } from "./Store/Store.jsx";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
