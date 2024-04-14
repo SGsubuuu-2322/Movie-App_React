71bf060ccb9b62901cfd71b01f8bf4e7ea2c2720
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-04-14 21:12:04 +0530

    Modified the horizental cards to handle the defficiency of data...

--- src/Components/Templates/HorizentalCards.jsx
+++ src/Components/Templates/HorizentalCards.jsx
@@ -8,32 +8,38 @@ const HorizentalCards = ({ data }) => {
   // console.log(data);
   return (
     <div className="w-[100%] flex overflow-y-hidden mb-5 p-5">
-      {data.map((d, i) => (
-        <Link
-          to={`/${d.media_type}/details/${d.id}`}
-          key={i}
-          className="min-w-[15%] h-[45vh] mr-5 mb-2 bg-zinc-900 pb-2 overflow-hidden"
-        >
-          <img
-            className="w-full h-[55%] object-cover"
-            src={`https://image.tmdb.org/t/p/original/${
-              d.backdrop_path || d.poster_path
-            }`}
-            alt=""
-          />
+      {data.length > 0 ? (
+        data.map((d, i) => (
+          <Link
+            to={`/${d.media_type}/details/${d.id}`}
+            key={i}
+            className="min-w-[15%] h-[45vh] mr-5 mb-2 bg-zinc-900 pb-2 overflow-hidden"
+          >
+            <img
+              className="w-full h-[55%] object-cover"
+              src={`https://image.tmdb.org/t/p/original/${
+                d.backdrop_path || d.poster_path
+              }`}
+              alt=""
+            />
 
-          <div className="text-white p-3 h-[45%] overflow-y-auto">
-            <h1 className=" mt-3 text-xl font-semibold ">
-              {" "}
-              {d.name || d.original_name || d.title || d.original_title}
-            </h1>
-            <p>
-              {d.overview.slice(0, 50)}...
-              <span className="text-zinc-500">more</span>
-            </p>
-          </div>
-        </Link>
-      ))}
+            <div className="text-white p-3 h-[45%] overflow-y-auto">
+              <h1 className=" mt-3 text-xl font-semibold ">
+                {" "}
+                {d.name || d.original_name || d.title || d.original_title}
+              </h1>
+              <p>
+                {d.overview.slice(0, 50)}...
+                <span className="text-zinc-500">more</span>
+              </p>
+            </div>
+          </Link>
+        ))
+      ) : (
+        <h1 className="text-3xl text-white font-black text-center mt-5">
+          Nothing to show...
+        </h1>
+      )}
     </div>
   );
 };
