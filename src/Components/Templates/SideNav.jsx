6e378cc9b9c10ef42c32ba0bb2c3feaf857a976e
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-05-12 22:43:50 +0530

    Added some relevant comments in the sideNav file...

--- src/Components/Templates/SideNav.jsx
+++ src/Components/Templates/SideNav.jsx
@@ -1,20 +1,27 @@
 // import React from 'react'
 
+// importing the Link properties from react-router-dom
 import { Link } from "react-router-dom";
 
+
+// This is the sidenav functional component...
 const SideNav = () => {
   return (
+
+    // This is the parent container....
     <div className="w-[20%] h-full border-zinc-400 border-r-2 p-10">
       <h1 className="text-2xl font-bold text-white">
         <i className="text-[#6556cd] ri-tv-fill mr-2"></i>
         <span className="text-2xl"> MovieApp.</span>
       </h1>
 
+{/* These are the navbar links... */}
       <nav className="flex flex-col text-zinc-400 text-xl gap-1">
         <h1 className="text-xl font-semibold text-white mt-10 mb-5">
           New Feeds
         </h1>
 
+{/* These are the links... */}
         <Link
           to="/trending"
           className="hover:bg-[#6556cd] hover:text-white duration-300 rounded-lg p-2"
@@ -72,4 +79,5 @@ const SideNav = () => {
   );
 };
 
+// Exporting it for using it elsewhere...
 export default SideNav;
